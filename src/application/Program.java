794cdddde2e55e2858b1cc11aa1d0384653b527b
Rafael de Luca <deluca1712@gmail.com>
2022-11-18 16:55:35 -0300

    Comparator usando classe anonima

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entitities.MeuComparator;
@@ -15,12 +16,31 @@ public class Program {
 		 list.add(new Produto("Celular", 950.00));
 		 list.add(new Produto("Tablet", 1500.50));
 		 list.add(new Produto("Capinha de Celular", 40.0));
+		 list.add(new Produto("Televisao", 800.0));
 		 
 		list.sort(new MeuComparator());
 		
-		System.out.println("Produtos ordenados por nome\n");
+		System.out.println("Produtos ordenados por nome");
 		for(Produto nickname : list) {
 			System.out.println(nickname);
+		}	
+		
+		// declarando comparator utilizando uma classe anônima
+		Comparator <Produto> comp = new Comparator<Produto>() {
+
+			@Override
+			public int compare(Produto p1, Produto p2) {
+				//comparando os produtos pelo preço
+				return p1.getPreco().compareTo(p2.getPreco());
+			}
+			
+		};
+		
+		list.sort(comp);
+		
+		System.out.println("\nProdutos ordenados por preco");
+		for(Produto nicknamePorPreco : list) {
+			System.out.println(nicknamePorPreco);
 		}	
 		 
 	 }
